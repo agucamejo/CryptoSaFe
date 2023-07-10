9af3a0fccc3dbc71f58edef7ffeed59a395a8485
Agustin Camejo <agustincamejo03@gmail.com>
2023-07-10 05:03:04 -0300

    LoginView diseñada

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,6 +1,27 @@
 <template>
-    <Div>
-        <p>Login</p>
-        <RouterLink to="/home">Ingresar</RouterLink>
-    </Div>
+    <div class="container">
+            
+        <div class="logo">
+            <img src="../components/icons/logo-cryptosafe.svg" alt="Logo de CryptoSaFe"/>
+            <div style="text-shadow: rgba(0, 0, 0, 0.25) 0 2px " class="titulos">
+                <span style="color: #F7931A; font-size: 36px">Crypto</span><span style="color: #00146B; font-size: 36px;">SaFe</span>
+            </div>
+        </div>
+
+        <div class="form">
+            <div class="inner-container">
+                <div class="box">
+                    <h1>Iniciar Sesion</h1>
+                    <input type="text" placeholder="Usuario"/>
+                    <input type="text" placeholder="Contraseña"/>
+                    <button><RouterLink to="/home" style="color: #000000; text-decoration: none;">Ingresar</RouterLink></button>
+                    <RouterLink to="/" style="color: #000000">Cancelar</RouterLink>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
+
+<script>
+import '../assets/LoginView.css'
+</script>
