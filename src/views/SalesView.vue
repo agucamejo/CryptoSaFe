edefd5d4ae4c2af14021d3ed883371901e1ae6b8
Agustin Camejo <agustincamejo03@gmail.com>
2023-07-17 06:28:37 -0300

    Sales y Purchase funcionando, falta store y css

--- src/views/SalesView.vue
+++ src/views/SalesView.vue
@@ -2,6 +2,7 @@
 import MenuBar from '../components/MenuBar.vue'
 import GeneralFooter from '../components/GeneralFooter.vue'
 import GeneralHeader from '../components/GeneralHeader.vue';
+import SalesSection from '../components/SalesSection.vue';
 </script>
 
 <template>
@@ -9,7 +10,9 @@ import GeneralHeader from '../components/GeneralHeader.vue';
     <div class="logo"></div>
     <div class="section-name"><GeneralHeader></GeneralHeader></div>
     <div class="menu"><MenuBar></MenuBar></div>
-    <div class="main-content"></div>
+    <div class="main-content">
+      <SalesSection />
+    </div>
     <div class="footer"><GeneralFooter></GeneralFooter></div>
   </div>
 </template>
