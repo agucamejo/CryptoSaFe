540ac5a061615a265a56b16a5e7f9053b2119173
Agustin Camejo <agustincamejo03@gmail.com>
2023-08-04 20:45:34 -0300

    Correción router links

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,4 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import { defineAsyncComponent } from 'vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,42 +6,42 @@ const router = createRouter({
     {
       path: '/',
       name: 'welcome',
-      component: defineAsyncComponent({ loader: () => import ('../views/WelcomeView.vue')})
+      component: () => import ('../views/WelcomeView.vue')
     },
     {
       path: '/login',
       name: 'login',
-      component: defineAsyncComponent({ loader: () => import ('../views/LoginView.vue')})
+      component: () => import ('../views/LoginView.vue')
     },
     {
       path: '/home',
       name: 'home',
-      component: defineAsyncComponent({ loader: () => import ('../views/HomeView.vue')})
+      component: () => import ('../views/HomeView.vue')
     },
     {
       path: '/purchase',
       name: 'purchase',
-      component: defineAsyncComponent({ loader: () => import ('../views/PurchaseView.vue')})
+      component:  () => import ('../views/PurchaseView.vue')
     },
     {
       path: '/sales',
       name: 'sales',
-      component: defineAsyncComponent({ loader: () => import ('../views/SalesView.vue')})
+      component: () => import ('../views/SalesView.vue')
     },
     {
       path: '/movements',
       name: 'movements',
-      component: defineAsyncComponent({ loader: () => import ('../views/MovementsView.vue')})
+      component: () => import ('../views/MovementsView.vue')
     },
     {
       path: '/balance',
       name: 'balance',
-      component: defineAsyncComponent({ loader: () => import ('../views/BalanceView.vue')})
+      component: () => import ('../views/BalanceView.vue')
     },
     {
       path: '/learn',
       name: 'learn',
-      component: defineAsyncComponent({ loader: () => import ('../views/LearnView.vue')})
+      component: () => import ('../views/LearnView.vue')
     },
     {
       path: '/help',
