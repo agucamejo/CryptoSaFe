ab72c2aa69f0be9d84ffa42f19e7bfddd9978136
Agustin Camejo <agustincamejo03@gmail.com>
2023-08-05 20:03:35 -0300

    Eliminacion codigo innecesario

--- src/services/apiClient.js
+++ src/services/apiClient.js
@@ -1,17 +1,8 @@
 import axios from 'axios';
 
-function apiClient() {
+export default function apiClient() {
   return axios.create({
     baseURL: 'https://labor3-d60e.restdb.io/rest/',
     headers: {'x-apikey': '64a2e9bc86d8c525a3ed8f63'}
   });
 }
-
-export default {
-  login(credentials) {
-    const { id, password } = credentials;
-    
-    return apiClient().post('/login', { user_id: id, password });
-  },
-};
-
